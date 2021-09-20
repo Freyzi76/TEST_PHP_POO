7de56f37102e6955607bbb0a377407d3bb0f2eee
hugo MARC <hugo.marc76113@gmail.com>
2021-09-20 14:33:52 +0200

    FIRST PUSH

---
+++ views/blog/index.php
@@ -0,0 +1,21 @@
+<h1>Homepage</h1>
+
+
+<p>
+    
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, error. 
+    Illo, quibusdam nobis? Officiis iure repudiandae voluptatum eos aliquid aut totam perspiciatis veniam, 
+    doloremque explicabo neque, repellat ex quidem molestiae veritatis quia, incidunt velit at assumenda! 
+    Rem tenetur voluptatibus nemo molestiae doloribus earum. Debitis labore sequi quod eaque! Quidem ad 
+    nisi culpa eum maxime provident. Reiciendis tenetur nemo, ab possimus corrupti quos eum dolores quasi quam maxime non dicta rerum. 
+    Ducimus, suscipit dignissimos unde impedit eaque minus quas dolorum, culpa nam placeat labore reiciendis corporis eos nisi earum totam 
+    pariatur dolorem temporibus nesciunt provident adipisci rem. Natus sit nulla reprehenderit tempore ipsam odit esse omnis mollitia saepe aut, c
+    ommodi unde dolore facere et voluptatum? Qui fugiat repellendus minima commodi est aliquam repudiandae quod totam error quaerat? Voluptates sint 
+    ullam asperiores at quisquam tempora, corporis ea, repellendus, architecto cumque culpa voluptatum. Voluptatibus voluptatem odit corporis tempora 
+    deserunt error possimus praesentium perferendis ab, alias, suscipit architecto dolor minima aliquid quibusdam laborum esse ipsa beatae explicabo, 
+    quia enim culpa! Nesciunt obcaecati nisi quis quaerat perferendis aliquam cum veniam debitis non quae ex nulla temporibus aspernatur recusandae voluptas 
+    dolores delectus quas odio at a quisquam animi, distinctio accusamus. Recusandae aliquam facilis nisi magni porro nihil, tempora sit ab voluptatibus eligendi 
+    nemo iusto sequi non voluptatem iste voluptatum corrupti dignissimos veniam tempore earum, est ipsum possimus velit quisquam. A asperiores ea voluptas iste blanditiis 
+    laboriosam minima aliquid illum nam quas. Voluptate nisi ea repellat laborum nobis non, fuga facilis. Mollitia pariatur iste nobis quidem. Ipsum?
+
+</p>
