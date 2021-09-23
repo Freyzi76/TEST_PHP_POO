3ee8d4688d9dafe58a46a4174842c30b9617e615
hugo MARC <hugo.marc76113@gmail.com>
2021-09-23 08:53:49 +0200

    MAJ

---
+++ src/views/admin/post/index.php
@@ -0,0 +1,43 @@
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Titre</th>
+      <th scope="col">Publié le</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+
+  <tbody>
+
+    <?php foreach ($params['posts'] as $post): ?>
+
+
+    <tr>
+
+      <th scope="row"><?= $post->id ?></th>
+
+      <td><?= $post->title ?></td>
+
+      <td><?= $post->getCreatedAt() ?></td>
+
+      <td>
+
+          <a href="/admin/posts/edit/<?= $post->id ?>" class="btn btn-warning">Modifier</a>
+
+          <form action="/admin/posts/delete/<?= $post->id ?>" method="post" class="d-inline">
+
+            <button type='submit' class="btn btn-danger">Supprimer</button>
+
+          </form>
+
+        
+        </td>
+
+    </tr>
+
+
+        <?php endforeach ?>
+    
+  </tbody>
+</table>
